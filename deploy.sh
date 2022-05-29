13ea618abbd8616d4eb89ee371b2fb43ac2040e2
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-05-29 14:10:32 -0400

    updating intro

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,5 @@ echo "deploy sphinx container"
 kubectl apply -f deployment.yaml --namespace r-mordasiewicz
 
 echo "kubectl exec --namespace r-mordasiewicz -it sphinx -c sphinx -- /bin/bash"
+
+echo "kubectl describe pod sphinx -n r-mordasiewicz"
