42d5ba545ef40f852803bfb540ae33d6ccfc1368
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-05-29 16:59:38 -0400

    updating intro

--- increment-version.sh
+++ increment-version.sh
@@ -6,6 +6,4 @@ set -e
 CONTAINERVERSION=`cat VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
 echo $CONTAINERVERSION > VERSION
 
-cat deployment.yaml | sed -re "s/image:[[:space:]]robinhoodis\/sphinx:.*/image: robinhoodis\/sphinx:${CONTAINERVERSION}/" > deployment.yaml.tmp && mv deployment.yaml.tmp deployment.yaml
-
 exit 0
